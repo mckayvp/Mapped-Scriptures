40365455c9962aed7b56b9bc95f01b1429ddd49c
McKay Palmer <mckaypalmer@gmail.com>
2018-11-07 19:32:23 -0700

    Initial Commit

---
+++ Project 3 Palmer McKay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 3 Palmer McKay
+//
+//  Created by McKay Palmer on 11/7/18.
+//  Copyright © 2018 McKay Palmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
