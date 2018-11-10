0379e89c09baf35602ad6fd4532fc96306c2a737
McKay Palmer <mckaypalmer@gmail.com>
2018-11-09 17:12:56 -0700

    Adding pin to map

--- Project 3 Palmer McKay/Controller/MapViewController.swift
+++ Project 3 Palmer McKay/Controller/MapViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController : UIViewController {
+class MapViewController : UIViewController, MKMapViewDelegate {
     
+    
+    // MARK: - Constants
+    
+    private struct Constant {
+        static let AnnotationIdentifier = "MapPin"
+    }
+    
+    // MARK: - Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
+    // MARK: - View Controller Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,5 +31,34 @@ class MapViewController : UIViewController {
             navigationItem.leftItemsSupplementBackButton = true
             navigationItem.leftBarButtonItem = splitVC.displayModeButtonItem    
         }
+        
+        mapView.register(MKPinAnnotationView.self,
+                         forAnnotationViewWithReuseIdentifier:
+                            Constant.AnnotationIdentifier)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = CLLocationCoordinate2DMake(40.2506, -111.65247)
+        annotation.title = "Tanner Building"
+        annotation.subtitle = "BYU Campus"
+        
+        mapView.addAnnotation(annotation)
+    }
+    
+    
+    // MARK: - Map View Delegate
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let view = mapView.dequeueReusableAnnotationView(withIdentifier:
+            Constant.AnnotationIdentifier,
+                                                         for: annotation)
+        
+        if let pinView = view as? MKPinAnnotationView {
+            pinView.canShowCallout = true
+            pinView.animatesDrop = true
+            pinView.pinTintColor = .blue 
+        }
+        
+        return view
     }
 }
